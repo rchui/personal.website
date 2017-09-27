a08e0b79ecd6226472800d0e578482e7bd92e6f9
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-26 22:50:18 -0500

    Deleted default content and added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Landing from './components/Landing/Landing.jsx';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Router>
+          <div className="Router">
+            <Route exact path="/" component={Landing} />
+          </div>
+        </ Router>
       </div>
     );
   }
